74cc1e7c768e3d704fe52a2356680e0b92de494c
Laura Pham <l.pham2311@gmail.com>
2020-06-03 12:50:50 +1000

    Added blog model and created routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'pages#home', as: 'landing'
+  get 'pages/blog', as: "blog"
+  get 'pages/contact', as: "contact"
+  get 'pages/about', as: "about"
+
+  resources :photos, only: [:show, :index, :destroy, :edit, :update]
+  resources :blogs, only: [:show, :index, :destroy, :edit, :update]
+  
 end
