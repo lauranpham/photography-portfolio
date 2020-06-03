16d09fe13b8514b2e715581cf5ddb189fccfbd14
Laura Pham <l.pham2311@gmail.com>
2020-06-03 12:59:31 +1000

    Added views and deleted unnecessary views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'pages#home', as: 'landing'
+  root to: 'pages#home', as: 'home'
   get 'pages/blog', as: "blog"
   get 'pages/contact', as: "contact"
   get 'pages/about', as: "about"
