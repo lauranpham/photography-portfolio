3aac96bfcf08f5d8452972bc86bc9f07a0d1888d
Laura Pham <l.pham2311@gmail.com>
2020-06-03 14:47:47 +1000

    Added navigation bar and background image

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+japan_count = 1
+10.times do 
+    Photo.create!(src: "japan" + japan_count.to_s + ".jpeg", name: "japan" + japan_count.to_s, description: "A photo walk through japan" )
+    japan_count += 1
+end 
+
